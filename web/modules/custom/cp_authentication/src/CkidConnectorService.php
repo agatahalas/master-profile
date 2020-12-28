44e8c53f731bf5775f627f666178995fd71ea59a
Agata Halas <agata.halas@circlekeurope.com>
2020-12-28 16:12:41 +0100

    CKID - Get Token method

--- web/modules/custom/cp_authentication/src/CkidConnectorService.php
+++ web/modules/custom/cp_authentication/src/CkidConnectorService.php
@@ -5,6 +5,7 @@ namespace Drupal\cp_authentication;
 
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Url;
+use GuzzleHttp\Exception\RequestException;
 
 class CkidConnectorService {
   /**
@@ -80,6 +81,45 @@ class CkidConnectorService {
     $request_url = Url::fromUri($uri, $option)->toString();
 
     return $request_url;
+  }
+
+  /**
+   * Get token.
+   *
+   * @param $code
+   * @return mixed
+   */
+  public function getToken($code) {
+    $uri = $this->getApiUrl() . '/api/v1/oauth/token';
+
+    $response = $this->httpClient->post($uri, [
+      'headers' => [
+        'Accept' => 'application/json',
+        'Content-Type' => 'application/x-www-form-urlencoded',
+        'Authorization' => 'Basic ' . base64_encode($this->clientId . ':' . $this->clientSecret),
+      ],
+      'form_params' => [
+        'code' => $code,
+        'grant_type' => 'authorization_code',
+        'scope' => 'USER',
+        'redirect_uri' => Url::fromRoute('cp_authentication.get_token', [], ['absolute' => TRUE, 'https' => TRUE])->toString(),
+      ],
+    ]);
+
+    $body = json_decode($response->getBody()->getContents());
+
+    if (!empty($body->access_token)) {
+      user_cookie_save([
+        'kid_token' => $body->access_token,
+      ]);
+    }
+
+    if ($response->getStatusCode() == 200) {
+      return TRUE;
+    }
+    else {
+      return FALSE;
+    }
   }
 
 }
